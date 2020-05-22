098a93649413c4667f4000a1af1381d569f4aeb5
Alan Rezepka <alanrezepka@mbp-de-alan.lan>
2020-05-22 17:17:59 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,3 @@
+
+var pulseras =["CAdqJw_nMYQ", "CATXynrn56K", "B_x30WdnmFZ", "B-poq9HHwFp", "B7Tv6Ernchx", "B-DHwP-nN-S"];
+var nombres = ["Combinado Otoño", "Combinado Rojo", "Combinado Negro", "Estrella Negra", "Celeste", "Tres Estrellas"]
